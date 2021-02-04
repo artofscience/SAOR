724fc02544b60c935a0dfed729a0770fa00329c0
Max van der Kolk <m.vanderkolk@uva.nl>
2021-02-04 08:51:32 +0100

    Move approximations to `sao` directory.
    The current base class for the approximations stores significant number
    of attributes, that could possibly be reduced further.

--- sao/problems/problem.py
+++ sao/problems/problem.py
@@ -2,8 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class Problem(ABC):
-    def __init__(self):
-        ...
+    def __init__(self, n, m, xmin, xmax):
+        self.n = n
+        self.m = m
+        self.xmin = xmin
+        self.xmax = xmax
 
     @abstractmethod
     def response(self, x):
--- sao/problems/rosenbrock.py
+++ sao/problems/rosenbrock.py
@@ -1,10 +1,12 @@
+import numpy as np
+
 from .problem import Problem
 from scipy.optimize import rosen, rosen_der
 
 
 class Rosenbrock(Problem):
     def __init__(self):
-        super().__init__()
+        super().__init__(1, 1, np.array(0), np.array(3))
 
     def response(self, x):
         return rosen(x)
