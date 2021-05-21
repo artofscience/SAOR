ae0588ec0b4c63660966d5d91f9028e15312e1f8
Giannis <i.pappas@tudelft.nl>
2021-05-21 19:10:26 +0300

    Fixed a bug in VDP beam; I was not taking into account the last constraint (the bad structure of Mixed class initialization showed up). Still I need to fix a bug in plotting responses of choice; right now `Plot3.plot_pair` method doesn't allow to plot whichever responses you choose.

--- examples/vanderplaats.py
+++ examples/vanderplaats.py
@@ -90,6 +90,9 @@ def example_vanderplaats(N):
         # x_k = solver.update()
         # solves += solver.iterin
 
+        # Assess convergence (give the correct keyword arguments for the criterion you chose)
+        criterion.assess_convergence(x_k=x_k, f=f, iter=itte, lam=lam, df=df)
+
         itte += 1
 
     logger.info('Optimization loop converged!')
@@ -108,43 +111,43 @@ def example_vanderplaats_mixed(N):
                1: np.arange(N, prob.n)}
     resp_set = {0: np.array([0]),
                 1: np.arange(1, N + 1),
-                2: np.arange(N + 1, prob.n),
-                3: np.array([prob.n])}
+                2: np.arange(N + 1, prob.n + 1),
+                3: np.array([prob.n + 1])}
 
     # Instantiate a mixed approximation scheme
     subprob_map = {
                    (0, 0): Subproblem(intervening=Linear(),
                                       approximation=Taylor1(),
                                       ml=MoveLimitIntervening(xmin=prob.xmin[var_set[0]],
-                                                              xmax=prob.xmax[var_set[0]], move_limit=0.2)),
+                                                              xmax=prob.xmax[var_set[0]])),
                    (0, 1): Subproblem(intervening=Linear(),
                                       approximation=Taylor1(),
                                       ml=MoveLimitIntervening(xmin=prob.xmin[var_set[1]],
-                                                              xmax=prob.xmax[var_set[1]], move_limit=0.2)),
+                                                              xmax=prob.xmax[var_set[1]])),
                    (1, 0): Subproblem(intervening=ConLin(),
                                       approximation=Taylor1(),
                                       ml=MoveLimitIntervening(xmin=prob.xmin[var_set[0]],
-                                                              xmax=prob.xmax[var_set[0]], move_limit=0.2)),
+                                                              xmax=prob.xmax[var_set[0]])),
                    (1, 1): Subproblem(intervening=ReciSquared(),
                                       approximation=Taylor1(),
                                       ml=MoveLimitIntervening(xmin=prob.xmin[var_set[1]],
-                                                              xmax=prob.xmax[var_set[1]], move_limit=0.2)),
+                                                              xmax=prob.xmax[var_set[1]])),
                    (2, 0): Subproblem(intervening=Linear(),
                                       approximation=Taylor1(),
                                       ml=MoveLimitIntervening(xmin=prob.xmin[var_set[0]],
-                                                              xmax=prob.xmax[var_set[0]], move_limit=0.2)),
+                                                              xmax=prob.xmax[var_set[0]])),
                    (2, 1): Subproblem(intervening=Linear(),
                                       approximation=Taylor1(),
                                       ml=MoveLimitIntervening(xmin=prob.xmin[var_set[1]],
-                                                              xmax=prob.xmax[var_set[1]], move_limit=0.2)),
+                                                              xmax=prob.xmax[var_set[1]])),
                    (3, 0): Subproblem(intervening=ConLin(),
                                       approximation=Taylor1(),
                                       ml=MoveLimitIntervening(xmin=prob.xmin[var_set[0]],
-                                                              xmax=prob.xmax[var_set[0]], move_limit=0.2)),
+                                                              xmax=prob.xmax[var_set[0]])),
                    (3, 1): Subproblem(intervening=ReciCubed(),
                                       approximation=Taylor1(),
                                       ml=MoveLimitIntervening(xmin=prob.xmin[var_set[1]],
-                                                              xmax=prob.xmax[var_set[1]], move_limit=0.2))
+                                                              xmax=prob.xmax[var_set[1]]))
                    }
 
     # Instantiate a mixed scheme
@@ -162,9 +165,11 @@ def example_vanderplaats_mixed(N):
 
     # Instantiate plotter
     plotter = Plot(['objective', 'constraint_1', 'max_constr_violation'], path=".")
-    plotter2_flag = False
-    if plotter2_flag:
-        plotter2 = Plot2(prob)
+    plotter3_flag = True
+    if plotter3_flag:
+        plotter3 = Plot3(prob,
+                         responses=np.array([resp_set[i][0] for i in range(len(resp_set.keys()))]),
+                         variables=np.arange(0, prob.n))
 
     # Initialize iteration counter and design
     itte = 0
@@ -183,18 +188,15 @@ def example_vanderplaats_mixed(N):
         logger.info(
             'iter: {:^4d}  |  obj: {:^9.3f}  |  constr1: {:^6.3f}  |  constr2: {:^6.3f}  |  '
             'constr3: {:^6.3f}  |  max_constr_viol: {:^6.3f}'.format(
-                itte, f[0], f[1], f[+1 + N], f[-1], max(0, max(f[1:]))))
-        # logger.info(
-        #     'iter: {:^4d}  |  obj: {:^9.3f}  |  constr1: {:^6.3f}  |  constr2: {:^6.3f}  |  constr3: {:^6.3f}'.format(
-        #         itte, f[0], f[1], f[1 + N], f[-1]))
+                itte, f[0], f[1], f[1 + N], f[-1], max(0, max(f[1:]))))
         plotter.plot([f[0], f[1], max(0, max(f[1:]))])
 
         # Build approximate sub-problem at X^(k)
         subprob.build(x_k, f, df)
 
         # Plot current approximation
-        if plotter2_flag:
-            plotter2.plot_pair(x_k, f, prob, subprob, itte)
+        if plotter3_flag:
+            plotter3.plot_pair(x_k, f, prob, subprob, itte)
 
         # Call solver (x_k, g and dg are within approx instance)
         x_k, y, z, lam, xsi, eta, mu, zet, s = solver.subsolv(subprob)
@@ -203,11 +205,14 @@ def example_vanderplaats_mixed(N):
         # x_k = solver.update()
         # solves += solver.iterin
 
+        # Assess convergence (give the correct keyword arguments for the criterion you chose)
+        criterion.assess_convergence(x_k=x_k, f=f, iter=itte, lam=lam, df=df)
+
         itte += 1
 
     logger.info('Optimization loop converged!')
 
 
 if __name__ == "__main__":
-    example_vanderplaats(100)
-    example_vanderplaats_mixed(100)
+    # example_vanderplaats(100)
+    example_vanderplaats_mixed(3)
