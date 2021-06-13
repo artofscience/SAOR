400b0b94509bb1092d74b8d229d7247f78fe48d3
aatmdelissen <2767694+aatmdelissen@users.noreply.github.com>
2021-06-13 12:57:10 +0200

    Added first version of mixed intervening variable

--- sao/intervening_variables/__init__.py
+++ sao/intervening_variables/__init__.py
@@ -1,11 +1,13 @@
+from .intervening import Intervening
 from .exponential import Linear, Exponential
 from .exponential import Reciprocal, ReciSquared, ReciCubed
 from .conlin import ConLin
 from .fitted import ReciFit, Bezier, PolyFit
 from .mma import MMA, MMASquared
+from .mixed_intervening import Mixed
 
 __all__ = [
         'Linear', 'ConLin', 'Exponential', 'Reciprocal', 'ReciSquared',
         'ReciCubed', 'ReciFit', 'Bezier', 'PolyFit',
-        'MMA', 'MMASquared'
+        'MMA', 'MMASquared', 'Mixed'
 ]
