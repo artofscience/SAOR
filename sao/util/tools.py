2a0d9d5f839c59efd94d3fe0831ea6ac21c7a28b
Stijn Koppen <s.koppen@tudelft.nl>
2021-07-13 15:53:16 +0200

    move fill_set_when_empty to util.tools

--- sao/util/tools.py
+++ sao/util/tools.py
@@ -14,3 +14,18 @@ def parse_to_list(*args):
         return list(var_in)
     else:
         return [var_in]
+
+
+def fill_set_when_emtpy(s, n):
+    """Returns ``set(s)`` or a ``set(0..n)`` if ``set(s)`` is the empty set."""
+    if s is None or s is ...:
+        return set(range(n))
+
+    try:
+        s = set(s)
+    except TypeError:
+        s = set([s])
+
+    if len(s) == 0:
+        return set(range(n))
+    return s
