Added the 2D problem `PaperProblem_2D` that will be used throughout the paper and an example for it. Slightly simplified `scaling.py`. Removed a bug from `plotter.py` and moved `plot_approx.py` to the `util` directory (outside the SAO package).

examples/paper_problem.py
@@ -1,15 +1,15 @@
 import numpy as np
 import logging
-from Problems.Square import Square
-from sao.approximations.taylor import Taylor1, Taylor2
+from Problems.PaperProblem_2D import PaperProblem2D
+from sao.approximations.taylor import *
 from sao.intervening_variables import *
 from sao.move_limits import *
 from sao.problems.subproblem import Subproblem
-from sao.solvers import *
-from sao.util.plotter import *
+from sao.solvers.SolverIP_Svanberg import SvanbergIP
+from sao.solvers.interior_point import InteriorPointXYZ as ipopt
+from sao.util import *
 from sao.convergence_criteria import *
 from sao.scaling_strategies.scaling import *
-
 
 # Set options for logging data: https://www.youtube.com/watch?v=jxmzY9soFXg&ab_channel=CoreySchafer
 logger = logging.getLogger(__name__)
@@ -24,65 +24,62 @@ logger.addHandler(stream_handler)
 np.set_printoptions(precision=4)
 
 
-def example_square(n):
-    logger.info("Solving Square.py using y=MMA and solver=SvanbergIP")
+def example_paper_problem():
+    logger.info("Solving test_poly using y=MMA and solver=Ipopt Svanberg")
 
     # Instantiate problem
-    prob = Square(n)
+    prob = PaperProblem2D()
 
     # Instantiate a non-mixed approximation scheme
     subprob = Subproblem(approximation=Taylor1(MMA(prob.xmin, prob.xmax)))
-    subprob.set_limits([TrustRegion(prob.xmin, prob.xmax), TrustRegion(move_limit=5.0)])
+    subprob.set_limits([Bound(prob.xmin, prob.xmax), MoveLimit(move_limit=0.5)])
 
     # Instantiate solver
     solver = SvanbergIP(prob.n, prob.m)
 
     # Instantiate convergence criterion
     # criterion = KKT(xmin=prob.xmin, xmax=prob.xmax)
-    # converged = ObjectiveChange(prob)
-    # converged = Feasibility(prob)
-
-    converged = ObjectiveChange(prob.f[0]) & Feasibility(prob.f[1:], slack=1e-3) | IterationCount(5)
-
-    # criterion = VariableChange(xmin=prob.xmin, xmax=prob.xmax)
+    # criterion = ObjectiveChange()
+    criterion = VariableChange(xmin=prob.xmin, xmax=prob.xmax)
     # criterion = Feasibility()
     # criterion = Alltogether(xmin=prob.xmin, xmax=prob.xmax)
 
     # Instantiate plotter
-    plotter = Plot(['objective', 'constraint', f'{criterion.__class__.__name__}', 'max_constr_violation'], path=".")
+    plotter = Plot(['objective', 'constraint', f'{criterion.__class__.__name__}', 'max_constr_violation'], path="../../../../Desktop")
     plotter2_flag = True
     if plotter2_flag:
-        plotter2 = Plot2(prob, responses=np.array([0, 1]), variables=np.arange(0, prob.n))
+        plotter2 = Plot2(prob)
 
     # Initialize iteration counter and design
     itte = 0
     x_k = prob.x0.copy()
 
     # Optimization loop
-    while not converged:
+    while not criterion.converged:
 
-        # Evaluate responses and sensitivities at current point, i.e. g(X^(k)), dg(X^(k))
+        # Evaluate responses and sensitivities at current point, i.e. g(X^(k)), dg(X^(k)), ddg(X^(k))
         f = prob.g(x_k)
         df = prob.dg(x_k)
         ddf = (prob.ddg(x_k) if subprob.approx.__class__.__name__ == 'Taylor2' else None)
 
         # Build approximate sub-problem at X^(k)
-        subprob.build(x_k, f, df, ddf)
+        subprob.build(x_k, f, df)
 
         # Plot current approximation
         if plotter2_flag:
-            plotter2.plot_pair(x_k, f, prob, subprob, itte)
+            # plotter2.plot_pair(x_k, f, prob, subprob, itte)
+            plotter2.contour_plot(x_k, f, prob, subprob, itte)
 
-        # Solve current subproblem
+        # Call solver (x_k, g and dg are within approx instance)
         x_k, y, z, lam, xsi, eta, mu, zet, s = solver.subsolv(subprob)
 
-        # Assess convergence (give the correct keyword arguments for the criterion you chose)
-        # criterion(x_k=x_k, obj=f[0], constraints=f[1:], iter=itte, lam=lam, df=df)
+        # Assess convergence (give the correct keyword arguments for the criterion you choose)
+        criterion.assess_convergence(x_k=x_k, f=f, iter=itte, lam=lam, df=df)
 
         # Print & Plot
-        logger.info('iter: {:^4d}  |  x: {:<20s}  |  obj: {:^9.3f}  |  constr: {:^6.3f}  |  criterion: {:^6.3f}  '
-                    '|  max_constr_viol: {:^6.3f}'.format(itte, np.array2string(x_k[0:2]), f[0], f[1],
-                                                          criterion.value, max(0, max(f[1:]))))
+        logger.info(
+            'iter: {:^4d}  |  x: {:<10s}  |  obj: {:^9.3f}  |  criterion: {:^6.3f}  |  max_constr_viol: {:^6.3f}'.format(
+                itte, np.array2string(x_k[0]), f[0], criterion.value, max(0, max(f[1:]))))
         plotter.plot([f[0], f[1], criterion.value, max(0, max(f[1:]))])
 
         itte += 1
@@ -90,32 +87,32 @@ def example_square(n):
     logger.info('Optimization loop converged!')
 
 
-def example_square_mixed(n):
-    logger.info("Solving Square.py using y=Mixed and solver=SvanbergIP")
+def example_paper_problem_mixed():
+    logger.info("Solving test_poly using y=MixedML and Ipopt Svanberg")
 
     # Instantiate problem
-    prob = Square(n)
+    prob = PaperProblem2D()
 
     # Instantiate a mixed intervening variable
-    mix = Mixed(prob.n, prob.m + 1, default=Linear())
-    mix.set_intervening(MMA(prob.xmin, prob.xmax), var=[0], resp=[1])
+    mix = Mixed(prob.n, prob.m + 1, default=MMA(prob.xmin, prob.xmax))
+    # mix.set_intervening(MMA(prob.xmin, prob.xmax), var=[0], resp=[1])
 
     # Instantiate a mixed approximation scheme
     subprob = Subproblem(approximation=Taylor1(mix))
-    subprob.set_limits([TrustRegion(prob.xmin, prob.xmax), TrustRegion(move_limit=0.5)])
+    subprob.set_limits([Bound(prob.xmin, prob.xmax), MoveLimit(move_limit=5.0)])
 
     # Instantiate solver
     solver = SvanbergIP(prob.n, prob.m)
 
     # Instantiate convergence criterion
-    # criterion = KKT(xmin=prob.xmin, xmax=prob.xmax)
-    criterion = ObjectiveChange(prob.f)
+    criterion = KKT(xmin=prob.xmin, xmax=prob.xmax)
+    # criterion = ObjectiveChange()
     # criterion = VariableChange(xmin=prob.xmin, xmax=prob.xmax)
     # criterion = Feasibility()
     # criterion = Alltogether(xmin=prob.xmin, xmax=prob.xmax)
 
     # Instantiate plotter
-    plotter = Plot(['objective', 'constraint', f'{criterion.__class__.__name__}', 'max_constr_violation'], path=".")
+    plotter = Plot(['objective', 'constraint', f'{criterion.__class__.__name__}', 'max_constr_violation'], path="../../../../Desktop")
     plotter3_flag = True
     if plotter3_flag:
         plotter3 = Plot3(prob)
@@ -138,11 +135,12 @@ def example_square_mixed(n):
         # Plot current approximation
         if plotter3_flag:
             plotter3.plot_pair(x_k, f, prob, subprob, itte)
+            plotter3.contour_plot(x_k, f, prob, subprob, itte)
 
         # Call solver (x_k, g and dg are within approx instance)
         x_k, y, z, lam, xsi, eta, mu, zet, s = solver.subsolv(subprob)
 
-        # Assess convergence (give the correct keyword arguments for the criterion you chose)
+        # Assess convergence (give the correct keyword arguments for the criterion you choose)
         criterion.assess_convergence(x_k=x_k, f=f, iter=itte, lam=lam, df=df)
 
         # Print & Plot
@@ -157,6 +155,5 @@ def example_square_mixed(n):
 
 
 if __name__ == "__main__":
-    example_square(20)
-    example_square_mixed(20)
-
+    example_paper_problem()
+    example_paper_problem_mixed()

sao/scaling_strategies/scaling.py
@@ -12,13 +12,18 @@ class Scaling(ABC):
 
     @abstractmethod
     def update_condition(self, f, df, **kwargs):
+        """This method implements a condition that decides whether re-scaling should be applied."""
         return NotImplementedError
 
     @abstractmethod
     def update_factor(self, f, df, **kwargs):
+        """This method updates the scaling factor, i.e. ``f_scaled = factor * f``."""
         return NotImplementedError
 
     def scale(self, f=None, df=None, **kwargs):
+        """ This method scales the response vector ``f`` and the sensitivity matrix ``df``
+        according to the `update_factor` method when ``update_condition`` is met.
+        """
         if self.update_condition(f, df):
             self.update_factor(f, df)
         return self.factor * f, (df.T * self.factor).T
@@ -26,17 +31,11 @@ class Scaling(ABC):
 
 class InitialObjectiveScaling(Scaling):
     """
-    This is an example of an implemented scaling_strategies class.
+    This is an example of an implemented ``Scaling`` class.
     Here, the objective is scaled wrt its value at the 0th iteration.
     """
 
     def __init__(self, nresp, **kwargs):
-        """
-        Initializes a scaling class that scales the objective wrt to its value at the 0th iteration.
-
-        :param nresp: Number of responses
-        :param kwargs: `scale_to` is a vector that holds a desired (scaled) value for each response
-        """
         super().__init__(nresp, **kwargs)
         self.scale_to = kwargs.get('scale_to', np.ones_like(self.factor))
 
@@ -53,18 +52,15 @@ class InitialObjectiveScaling(Scaling):
             return False
 
 
-class InitialResponseScaling(Scaling):
+class InitialResponseScaling(InitialObjectiveScaling):
     """
     This class scales the objective and the constraints with respect to their value at the 0th iteration.
     This means that for the vector of responses `f` with f.shape = (m+1,) we have a respective vector `factor`, where:
         factor.shape = (m+1,)
-        factor[0] = scale_to / f[0]
-        f_scaled[j] = f[j] / norm(df[j, :]) , for j = 1,...,m
+        factor[0] = scale_to[0] / f[0]
+        factor[j] = scale_to[j] / norm(df[j, :]) , for j = 1,...,m
+        f_scaled[j] = factor[j] * f[j]           , for j = 0,...,m
     """
-
-    def __init__(self, nresp, **kwargs):
-        super().__init__(nresp, **kwargs)
-        self.scale_to = kwargs.get('scale_to', np.ones_like(self.factor))
 
     def update_factor(self, f, df, **kwargs):
         """
@@ -88,9 +84,3 @@ class InitialResponseScaling(Scaling):
                 raise ZeroDivisionError(f'Cannot use {self.__class__.__name__} class when ||dg_j/dx|| = 0')
             self.factor[j] = self.scale_to[j] / constr_norm
         return self
-
-    def update_condition(self, f, df, **kwargs):
-        if np.all(self.factor == 1):
-            return True
-        else:
-            return False