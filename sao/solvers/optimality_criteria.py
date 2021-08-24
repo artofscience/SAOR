1d561bc727f56a8e3943f20b085d8b46205d0f7e
Stijn Koppen <s.koppen@tudelft.nl>
2021-08-24 09:43:14 +0200

    updated version of oc (2 versions)

--- sao/solvers/optimality_criteria.py
+++ sao/solvers/optimality_criteria.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def oc(dg, x=None, gt=0, move=0.2, tol=1e-3, lower=0, upper=1e9):
+def oc(x, dg, gt=0, move=0.2, tol=1e-3, lower=0, upper=1e9):
     x_new = np.zeros_like(x)
     while (upper - lower) / (lower + upper) > tol:
         middle = (lower + upper) / 2
@@ -12,4 +12,18 @@ def oc(dg, x=None, gt=0, move=0.2, tol=1e-3, lower=0, upper=1e9):
             lower = middle
         else:
             upper = middle
-    return x_new, gt
+    return x_new, var_change
+
+
+def oc_simple(x, dg, move=0.2, tol=1e-3, lower=0, upper=1e9):
+    x_new = np.zeros_like(x)
+    while (upper - lower) / (lower + upper) > tol:
+        middle = (lower + upper) / 2
+        x_new[:] = x * np.sqrt(-dg[0] / dg[1] / middle)
+        x_new[:] = np.clip(np.clip(x_new, x - move, x + move), 0, 1)
+        var_change = np.sum((dg[1] * (x_new - x)))
+        if var_change > 0:
+            lower = middle
+        else:
+            upper = middle
+    return x_new
