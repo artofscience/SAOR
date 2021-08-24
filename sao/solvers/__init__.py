4baa0fb51c532c1ba210beea19ca87e82df6a4de
Stijn Koppen <s.koppen@tudelft.nl>
2021-08-24 12:21:04 +0200

    solvers init update

--- sao/solvers/__init__.py
+++ sao/solvers/__init__.py
@@ -1,3 +1,3 @@
 from sao.solvers.primal_dual_interior_point import pdip
-from sao.solvers.optimality_criteria import oc
+from sao.solvers.optimality_criteria import oc, oc1999, oc2010
 from .SolverIP_Svanberg import SvanbergIP
