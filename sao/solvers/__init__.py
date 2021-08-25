2521c3fc7464a35cef5ad96904c9a5ff8ab33351
Giannis <i.pappas@tudelft.nl>
2021-08-25 12:06:57 +0300

    Fixed ImportError

--- sao/solvers/__init__.py
+++ sao/solvers/__init__.py
@@ -1,4 +1,4 @@
 from sao.solvers.primal_dual_interior_point import pdip
-from sao.solvers.optimality_criteria import oc, oc1999, oc2010
+from sao.solvers.optimality_criteria import oc, oc1999
 from .SolverIP_Svanberg import SvanbergIP
 from sao.solvers.method_of_moving_asymptotes import mma
