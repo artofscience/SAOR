6b05b9e95d66f03877bbc58388fa6ccc6dda9eb5
Giannis <i.pappas@tudelft.nl>
2021-08-26 12:55:29 +0300

    Added an updated version of `scipy`. Everything should work now.

--- sao/solvers/__init__.py
+++ sao/solvers/__init__.py
@@ -3,3 +3,4 @@ from sao.solvers.optimality_criteria import oc, oc1999
 from sao.solvers.SolverIP_Svanberg import SvanbergIP
 from sao.solvers.method_of_moving_asymptotes import mma
 from sao.solvers.cvxopt_wrapper import CVXOPT
+from sao.solvers.scipy_wrapper import SCIPY
