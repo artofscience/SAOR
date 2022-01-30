c8c4cb470c50334d1b7709b4d36bbdcba6629f39
Stijn Koppen <s.koppen@tudelft.nl>
2022-01-30 16:36:43 +0100

    test conlin

--- tests/mappings/test_mapping.py
+++ tests/mappings/test_mapping.py
@@ -2,6 +2,7 @@ from problems.n_dim.square import Square
 from sao.mappings.mapping import LinearApproximation as LA
 from sao.mappings.mapping import DiagonalQuadraticApproximation as DQA
 from sao.mappings.mapping import Exponential as Exp
+from sao.mappings.mapping import ConLin
 from sao import intervening_variables, approximations
 import numpy as np
 import pytest
@@ -205,3 +206,33 @@ def test_ta2_rec(dx=1, tol=1e-4):
     assert f + np.sum(new.g(y), 1) == pytest.approx(old.g(y), tol)
     assert new.dg(y) == pytest.approx(old.dg(y), tol)
     assert new.ddg(y) == pytest.approx(old.ddg(y), tol)
+
+
+def test_conlin(dx=1, tol=1e-4):
+    prob = Square(10)
+    f = prob.g(prob.x0)
+    df = prob.dg(prob.x0)
+    inter = ConLin()
+    inter.update(prob.x0, df)
+    lin = Exp(p=1)
+    rec = Exp(p=-1)
+
+    # Check g(x) for ConLin, Linear(), and Reciprocal()
+    assert inter.g(prob.x0)[0, :] == pytest.approx(lin.g(prob.x0), rel=1e-4)
+    assert inter.g(prob.x0)[1, :] == pytest.approx(rec.g(prob.x0), rel=1e-4)
+    assert lin.g(prob.x0) == pytest.approx(prob.x0, rel=1e-4)
+    assert rec.g(prob.x0) == pytest.approx(1 / prob.x0, rel=1e-4)
+
+    assert inter.dg(prob.x0)[0, :] == pytest.approx(lin.dg(prob.x0), rel=1e-4)
+    assert inter.dg(prob.x0)[1, :] == pytest.approx(rec.dg(prob.x0), rel=1e-4)
+    assert lin.dg(prob.x0) == pytest.approx(np.ones_like(prob.x0), rel=1e-4)
+    assert rec.dg(prob.x0) == pytest.approx(-1 / prob.x0 ** 2, rel=1e-4)
+
+    assert inter.ddg(prob.x0)[0, :] == pytest.approx(lin.ddg(prob.x0), rel=1e-4)
+    assert inter.ddg(prob.x0)[1, :] == pytest.approx(rec.ddg(prob.x0), rel=1e-4)
+    assert lin.ddg(prob.x0) == pytest.approx(np.zeros_like(prob.x0), abs=1e-4)
+    assert rec.ddg(prob.x0) == pytest.approx(2 / prob.x0 ** 3, rel=1e-4)
+
+
+if __name__ == "__main__":
+    test_conlin()
