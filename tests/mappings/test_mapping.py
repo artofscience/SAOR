b0b00b8a32520454c211666771d90a556e376201
Stijn Koppen <s.koppen@tudelft.nl>
2022-02-01 20:26:07 +0100

    introduced new and simplified mixed mapping

--- tests/mappings/test_mapping.py
+++ tests/mappings/test_mapping.py
@@ -4,7 +4,7 @@ from sao.mappings.approximations import DiagonalQuadraticApproximation as DQA
 import pytest
 from sao.mappings.mapping import MixedMapping as MM
 from sao.mappings.mapping import MixedMappingNew as MMN
-
+import numpy as np
 
 def test_mm(dx=1, tol=1e-4):
     prob = Square(2)
@@ -38,18 +38,17 @@ def test_mmn(dx=1, tol=1e-4):
     ddf = prob.ddg(x)
 
     mm = MMN(prob.n, prob.m + 1)
-    mm[[0], [1]] = Exp(-1)
-    mm[[1], [1]] = Exp(-2)
+    mm[np.array([0]), [0, 1, 2]] = Exp(-1)
+    mm[[1], [2]] = Exp(-2)
 
     mm.update(prob.x0, df, ddf)
 
     aoa = DQA()
     aoa.update(x, df, ddg0=mm.ddg(x))
 
-    assert mm.g(x)[0, 0] == pytest.approx(x[0], tol)
-    assert mm.g(x)[1, 0] == pytest.approx(x[0], tol)
-    assert mm.g(x)[0, 1] == pytest.approx(1 / x[1], tol)
-    assert mm.g(x)[1, 1] == pytest.approx(1 / (x[1]) ** 2, tol)
+    assert mm.g(x)[0, [0, 1, 2]] == pytest.approx(1 / x, tol)
+    assert mm.g(x)[1, [0, 1]] == pytest.approx(x[0:2], tol)
+    assert mm.g(x)[1, 2] == pytest.approx(1 / (x[2]) ** 2, tol)
 
     assert aoa.ddg0 == pytest.approx(mm.ddg(x), tol)
 
